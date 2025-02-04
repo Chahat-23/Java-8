fcf440670d6b5c0570d12cedec4c80222b9ec44b
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-02-04 14:51:25 +0530

    predicate API

---
+++ src/FunctionalInterfacesDemo/PredicateDemo.java
@@ -0,0 +1,11 @@
+package FunctionalInterfacesDemo;
+
+import java.util.function.Predicate;
+
+public class PredicateDemo {
+    public static void main(String[] args) {
+        Predicate<Integer> predicate = t -> t % 2 == 0;
+        System.out.println(predicate.test(5));
+        System.out.println(predicate.test(4));
+    }
+}
