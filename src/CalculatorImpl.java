f6d74e78043dacc8abe234e5062253cc884d0be5
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-02-04 12:34:16 +0530

    Java 8 Lambda expression

---
+++ src/CalculatorImpl.java
@@ -0,0 +1,13 @@
+interface calculator {
+//    void switchOn();
+}
+
+public class CalculatorImpl{
+    //() -> {}; - syntax of lambda exp.
+    public static void main(String[] args) {
+//        calculator calc =() -> {
+//            System.out.println("Lambda expression");
+//        };
+//        calc.switchOn();
+    }
+}
