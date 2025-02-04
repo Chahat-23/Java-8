2dfc567b806ec2ff76bae3513ecc4b929fbf4f54
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-02-04 14:55:03 +0530

    predicate API with stream filter

--- src/FunctionalInterfacesDemo/PredicateDemo.java
+++ src/FunctionalInterfacesDemo/PredicateDemo.java
@@ -1,11 +1,18 @@
 package FunctionalInterfacesDemo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class PredicateDemo {
     public static void main(String[] args) {
-        Predicate<Integer> predicate = t -> t % 2 == 0;
-        System.out.println(predicate.test(5));
-        System.out.println(predicate.test(4));
+        //predicate with lambda expression
+//        Predicate<Integer> predicate = t -> t % 2 == 0;
+//        System.out.println(predicate.test(5));
+//        System.out.println(predicate.test(4));
+
+        //Stream API for filter
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        list.stream().filter(t->t%2==0).forEach(t -> System.out.println(t));
     }
 }
