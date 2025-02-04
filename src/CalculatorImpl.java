9a89b4ad0d52914a99d98c307a08c37859a82d1c
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-02-04 12:39:54 +0530

    Java 8 Lambda expression basic done

--- src/CalculatorImpl.java
+++ src/CalculatorImpl.java
@@ -1,5 +1,7 @@
 interface calculator {
 //    void switchOn();
+//void sum(int input);
+    int substract(int i1, int i2);
 }
 
 public class CalculatorImpl{
@@ -9,5 +11,20 @@ public class CalculatorImpl{
 //            System.out.println("Lambda expression");
 //        };
 //        calc.switchOn();
+
+//        calculator calc1 = (input) -> System.out.println(input);
+//        calc1.sum(5);
+
+        calculator calc2 = (i1, i2) -> {
+            if(i2>i1) {
+                return i2-i1;
+            }
+            else {
+                throw new ArithmeticException();
+            }
+        };
+        System.out.println(calc2.substract(2, 3));
+        System.out.println(calc2.substract(4, 3));
+
     }
 }
