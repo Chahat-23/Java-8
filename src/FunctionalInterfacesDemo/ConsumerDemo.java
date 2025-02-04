7a8fbba21342b016d3f419c6d68258916505d8c9
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-02-04 14:48:25 +0530

    consumer interface & stream API

--- src/FunctionalInterfacesDemo/ConsumerDemo.java
+++ src/FunctionalInterfacesDemo/ConsumerDemo.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerDemo {
-//
+// 1st basic approach
 //    @Override
 //    public void (Integer t)
 //    {
@@ -14,10 +14,14 @@ public class ConsumerDemo {
 //    }
 
     public static void main(String[] args) {
-        Consumer<Integer> consumer = (t) -> System.out.println(t);
-        consumer.accept(10);
+        //2nd approach with stream API
+//        Consumer<Integer> consumer = (t) -> System.out.println(t);
+//        consumer.accept(10);
 
         List<Integer> l1 = Arrays.asList(1,2,3,4,5);
-        l1.stream().forEach(consumer);
+//        l1.stream().forEach(consumer);
+
+        //3rd approach
+        l1.stream().forEach(t -> System.out.println(t));
     }
 }
